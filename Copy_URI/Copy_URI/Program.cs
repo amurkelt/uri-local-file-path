bb1f827288d29ab54c86999414029bcf2eb44926
ataras <ataras.md@gmail.com>
2023-07-16 22:56:22 +0300

    initial commit

---
+++ Copy_URI/Copy_URI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AbsoluteUriGenerator
+{
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                var executableName = typeof(Program).Assembly.GetName().Name + ".exe";
+                Console.WriteLine("Usage: {0} pathToConvert", executableName);
+                Console.WriteLine("Example: {0} \"C:\\foo\"", executableName);
+                return 1;
+            }
+
+            var path = args[0];
+            Console.WriteLine("Resolving file:// URI of path: {0}", path);
+            var uri = new Uri(path).AbsoluteUri;
+            var name = Path.GetFileName(path);
+
+            Console.WriteLine("Setting clipboard to resolved file:// URI: {0}", uri);
+            Clipboard.SetText("[" + name + "](" + uri + ")");
+            return 0;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Gives you link to any file on your computer.
+
+1. Press "Win+R"
+2. Type "shell:sendto"
+3. Click "Ok"
+4. Copy "Copy_URI.exe" to your "shell:sendto" folder
+5. Mouse right click on any file -> "Send-to" -> "Copy_URI"
+6. Your link to extenal file is ready to go.
